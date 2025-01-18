edece9d180d24b2e3d71cd3ac8215535ab9d647f
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2025-01-18 23:08:04 +0530

    Update part_2.rs
    added closure, lambda, unnamed func, how hashmap works and their associated methods

--- part_2.rs
+++ part_2.rs
@@ -1,3 +1,7 @@
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+use std::iter::zip;
+
 fn main() {
     println!("Hello, world!");
 
@@ -39,7 +43,7 @@ fn main() {
 
     println!("{:?}", an_vec[2]);
 
-    for i in &an_vec{
+    for i in &an_vec {
         println!("{}", i);
     }
 
@@ -88,12 +92,12 @@ fn main() {
 
     //heterogeneous vector
 
-    let mut het_vec:Vec<I32OrString> = Vec::new();
+    let mut het_vec: Vec<I32OrString> = Vec::new();
 
     het_vec.push(I32OrString::I32(44));
     het_vec.push(I32OrString::String("hello".to_string()));
 
-    for i in &het_vec{
+    for i in &het_vec {
         let mut data = match i {
             I32OrString::I32(i) => i.to_string(),
             I32OrString::String(i) => i.to_string(),
@@ -101,10 +105,161 @@ fn main() {
 
         println!("data -> {:?}", data);
     }
+
+    //hashmaps
+
+    let mut hashmap = HashMap::new();
+
+    hashmap.insert("john", 29);
+    hashmap.insert("ketty", 18);
+    hashmap.insert("warn", 21);
+    hashmap.insert("meet", 33);
+
+    println!("{:?}", hashmap);
+
+    let age = hashmap.get("john");
+
+    match age {
+        Some(&age) => println!("john: {}", age),
+        None => println!("no john"),
+    }
+
+    if let Some(age) = hashmap.get_mut("ketty") {
+        *age += 1; // Increment Alice's age
+    }
+
+    println!("{:?}", hashmap);
+
+    if hashmap.contains_key("john") {
+        println!("john: {}", hashmap["john"]);
+    }
+
+    let removed = hashmap.remove("ketty");
+
+    match removed {
+        Some(x) => println!("ketty: {}", x),
+        None => println!("ketty not found"),
+    }
+
+    println!("{:?}", hashmap);
+
+    for (k, v) in &hashmap {
+        println!("{}: {}", k, v);
+    }
+
+    for (_, age) in hashmap.iter_mut() {
+        *age += 1; // Increment everyone's age
+    }
+
+    println!("{:?}", hashmap);
+
+    println!("{:?}", hashmap.len());
+
+    println!("{:?}", hashmap.keys());
+
+    println!("{:?}", hashmap.values());
+
+    //updating a values based on key
+
+    match hashmap.entry("meet") {
+        // if key is exist
+        Entry::Occupied(mut entry) => {
+            *entry.get_mut() += 10;
+        }
+        // if key is not exist then make a key and insert value
+        Entry::Vacant(entry) => {
+            entry.insert(24);
+        }
+    }
+
+    // shorthand of above match arm code
+
+    hashmap.entry("mark").or_insert(28);
+
+    println!("{:?}", hashmap);
+
+    println!("{:?}", hashmap);
+
+    let mut another_map = HashMap::new();
+
+    another_map.insert("penny", 12);
+    another_map.insert("jolly", 16);
+
+    hashmap.extend(another_map);
+
+    println!("{:?}", hashmap);
+
+    //collecting into a hashmap
+
+    let names = vec!["tory", "jenny", "castle"];
+    let ages = vec![12, 16, 19, 25];
+
+    let zipped: HashMap<_, _> = names.into_iter().zip(ages.into_iter()).collect();
+
+    hashmap.extend(zipped);
+
+    println!("{:?}", hashmap);
+
+    hashmap.clear();
+
+    println!("{:?}", hashmap.is_empty());
+
+    //another way to define a hashmap
+
+    let map = HashMap::from([("Alice", 30), ("Bob", 25), ("Charlie", 35)]);
+
+    println!("{:?}", map);
+
+    //closure
+
+    let add = |a: i32, b: i32| -> i32 { a + b };
+
+    println!("add of closure fun -> {}", add(10, 40));
+
+    //lambda
+    let square = |a| a * a;
+    println!("square of lambda -> {:?}", square(4));
+
+    // unnamed function
+    let multiply = |a, b| a * b;
+    println!("multiply of unnamed function -> {:?}", multiply(5, 4));
+
+    let res = apply_closure(square, 10);
+
+    println!("closure function as param -> {:?}", res);
+
+    let mut y = 14;
+
+    // Closure captures `y` by mutable reference
+    let add_and_increment_y = |x| {
+        y += 1;
+        x + y
+    };
+
+    let result = apply_closure_mut(add_and_increment_y, 5);
+
+    println!("Result: {}", result); // Prints "Result: 16"
+    println!("y: {}", y); // Prints "y: 11"
+}
+
+//Closure Capturing by Mutable Reference (FnMut)
+fn apply_closure_mut<F>(mut f: F, x: i32) -> i32
+where
+    F: FnMut(i32) -> i32,
+{
+    f(x)
+}
+
+//passing closure func as args
+fn apply_closure<F>(f: F, x: i32) -> i32
+where
+    F: Fn(i32) -> i32,
+{
+    f(x)
 }
 
 #[derive(Debug)]
-enum I32OrString{
+enum I32OrString {
     I32(i32),
     String(String),
 }
@@ -118,7 +273,7 @@ impl I32OrString {
     }
 }
 
-fn even_filter(vc:Vec<i32>) -> Vec<i32> {
+fn even_filter(vc: Vec<i32>) -> Vec<i32> {
     return vc.into_iter().filter(|&x| x % 2 == 0).collect();
 }
 
